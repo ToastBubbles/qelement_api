10dc2c376584484597bf9df5d543a479e51a0afb
ToastBubbles <jeffneal11@gmail.com>
2023-12-11 14:57:12 -0600

    imp user prefs

--- src/controllers/color.controller.ts
+++ src/controllers/color.controller.ts
@@ -44,7 +44,7 @@ export class ColorsController {
     try {
       let thisObj = await this.colorsService.findByIdAll(data.id);
       if (thisObj) {
-        thisObjate({
+        thisObj.update({
           approvalDate: new Date().toISOString().slice(0, 23).replace('T', ' '),
         });
         return { code: 200, message: `approved` };
